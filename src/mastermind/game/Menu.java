f4d8888a664d1903878c05c42e4c4181b2afd3c7
AdamSahlen93 <72616481+AdamSahlen93@users.noreply.github.com>
2020-11-09 18:57:29 +0100

    Started Menu

--- src/mastermind/game/Menu.java
+++ src/mastermind/game/Menu.java
@@ -1,7 +1,33 @@
 package mastermind.game;
+import java.util.Scanner;
 
 public class Menu
 {
+    //Attributes controlling the program/menu
+    boolean runProgram = true;
+    Scanner playerInput = new Scanner(System.in);
+
     //Switch
+    public void runMenu()
+    {
+        int playerInputForMenu = playerInput.nextInt();
+        
+        switch (playerInputForMenu)
+        {
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            case 3:
+                System.out.println("3");
+                break;
+            case 99:
+                runProgram = false;
+            default:
+
+        }
+    }
 
 }
